e3392769787eb1e9772d509529807d1a18b9f697
Brett Jory <dev.bjory@gmail.com>
2019-09-11 12:45:32 -0700

    deleted unneeded folders

--- follow/app.js
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const myButton = React.creatElement('button', null, 'Click Me');
-console.log(JSON.stringify(myButton));
-
-ReactDOM.render(myButton, document.getElementById('app'));
